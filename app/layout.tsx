b945ba21f41fde05bdb440c2c24e68d5e4337030
mano-sudo <romancaseres929@gmail.com>
2026-05-06 13:21:42 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <AppNavbar />
           <CollaborativeCursors />
           <FloatingShootToggleHost />
